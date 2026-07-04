56788c7eb5394b21d2c99ffd5e1a914f6450dc35
Rohanskadu7309 <rohanskadu7309@gmai.com>
2026-07-04 20:05:35 +0530

    feat(accounts): implement custom email-based user authentication

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+
+    first_name = models.CharField(max_length=100)
+
+    last_name = models.CharField(max_length=100)
+
+    is_staff = models.BooleanField(default=False)
+
+    is_active = models.BooleanField(default=True)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        db_table = "users"
+
+    def __str__(self):
+        return self.email
