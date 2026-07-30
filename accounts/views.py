ea7de900a0df465c53c00282b0eb828112d15ba6
Rohanskadu7309 <rohanskadu7309@gmai.com>
2026-07-30 18:08:48 +0530

    feat(auth): implement register API view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import RegisterSerializer
+from .services import AuthenticationService
+
+
+class RegisterAPIView(APIView):
+
+    permission_classes = []
+
+    authentication_classes = []
+
+    def post(self, request):
+
+        serializer = RegisterSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        user = AuthenticationService.register(
+            serializer.validated_data
+        )
+
+        return Response(
+            {
+                "success": True,
+                "message": "User registered successfully.",
+                "data": {
+                    "id": user.id,
+                    "email": user.email,
+                },
+            },
+            status=status.HTTP_201_CREATED,
+        )
