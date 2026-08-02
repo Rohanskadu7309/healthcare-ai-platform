3d23388bde12f4ef443444414a04fa82b3e66eb8
Rohanskadu7309 <rohanskadu7309@gmai.com>
2026-08-02 18:06:23 +0530

    feat(auth): add change password API

--- accounts/views.py
+++ accounts/views.py
@@ -5,8 +5,8 @@ from drf_spectacular.utils import extend_schema, OpenApiResponse
 
 from common.responses import APIResponse
 
-from .serializers import RegisterSerializer, LoginSerializer, RefreshTokenSerializer, LogoutSerializer, ProfileSerializer, UpdateProfileSerializer
-from .services import RegistrationService, LoginService, RefreshTokenService, LogoutService, ProfileService
+from .serializers import RegisterSerializer, LoginSerializer, RefreshTokenSerializer, LogoutSerializer, ProfileSerializer, UpdateProfileSerializer, ChangePasswordSerializer
+from .services import RegistrationService, LoginService, RefreshTokenService, LogoutService, ProfileService, ChangePasswordService
 
 
 
@@ -177,4 +177,29 @@ class ProfileAPIView(APIView):
         return APIResponse.success(
             message="Profile updated successfully.",
             data=response_serializer.data,
+        )
+        
+        
+class ChangePasswordAPIView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        tags=["Authentication"],
+        request=ChangePasswordSerializer,
+        responses={
+            200: OpenApiResponse(description="Password changed successfully."),
+            400: OpenApiResponse(description="Validation error"),
+            401: OpenApiResponse(description="Authentication failed."),
+        },
+    )
+    def post(self, request):
+        
+        serializer = ChangePasswordSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        ChangePasswordService.change_password(request.user, serializer.validated_data)
+        
+        return APIResponse.success(
+            message="Password changed successfully.",
         )
