2bf5f50c9dc7a1311525adbdbb0cf3066efe430c
Rohanskadu7309 <rohanskadu7309@gmai.com>
2026-08-02 10:37:25 +0530

    feat(common): add API response utility

---
+++ common/responses.py
@@ -0,0 +1,37 @@
+from rest_framework import status
+from rest_framework.response import Response
+
+
+class APIResponse:
+
+    @staticmethod
+    def success(
+        message,
+        data=None,
+        status_code=status.HTTP_200_OK,
+    ):
+
+        return Response(
+            {
+                "success": True,
+                "message": message,
+                "data": data,
+            },
+            status=status_code,
+        )
+
+    @staticmethod
+    def error(
+        message,
+        errors=None,
+        status_code=status.HTTP_400_BAD_REQUEST,
+    ):
+
+        return Response(
+            {
+                "success": False,
+                "message": message,
+                "errors": errors,
+            },
+            status=status_code,
+        )
