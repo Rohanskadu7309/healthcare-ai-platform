a9c6f49ca441c4c10a2f80f87ed14097c13385e6
Rohanskadu7309 <rohanskadu7309@gmai.com>
2026-08-02 10:33:25 +0530

    feat(common): create common application

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'common',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'drf_spectacular',
