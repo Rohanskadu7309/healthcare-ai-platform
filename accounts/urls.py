419feb129b390bf62895ca39b0ef15348f742340
Rohanskadu7309 <rohanskadu7309@gmai.com>
2026-08-02 14:31:44 +0530

    feat(auth): register profile endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import RegisterAPIView, LoginAPIView, RefreshTokenAPIView, LogoutAPIView
+from .views import RegisterAPIView, LoginAPIView, RefreshTokenAPIView, LogoutAPIView, ProfileAPIView
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="register",),
     path("login/", LoginAPIView.as_view(), name="login"),
     path("refresh/", RefreshTokenAPIView.as_view(), name="refresh"),
     path("logout/", LogoutAPIView.as_view(), name="logout"),
+    path("profile/", ProfileAPIView.as_view(), name="profile"),
 ]
