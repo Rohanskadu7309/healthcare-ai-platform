58ef228fad10125b92a367283dc962129d4113dd
Rohanskadu7309 <rohanskadu7309@gmai.com>
2026-08-05 20:39:25 +0530

    feat(auth): add forgot password API

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import RegisterAPIView, LoginAPIView, RefreshTokenAPIView, LogoutAPIView, ProfileAPIView, ChangePasswordAPIView
+from .views import RegisterAPIView, LoginAPIView, RefreshTokenAPIView, LogoutAPIView, ProfileAPIView, ChangePasswordAPIView, ForgotPasswordAPIView
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="register",),
@@ -9,4 +9,5 @@ urlpatterns = [
     path("logout/", LogoutAPIView.as_view(), name="logout"),
     path("profile/", ProfileAPIView.as_view(), name="profile"),
     path("change-password/", ChangePasswordAPIView.as_view(), name="change-password"),
+    path("forgot-password/", ForgotPasswordAPIView.as_view(), name="forgot-password"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,12 @@
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from drf_spectacular.utils import extend_schema, OpenApiResponse
 
 from common.responses import APIResponse
 
-from .serializers import RegisterSerializer, LoginSerializer, RefreshTokenSerializer, LogoutSerializer, ProfileSerializer, UpdateProfileSerializer, ChangePasswordSerializer
-from .services import RegistrationService, LoginService, RefreshTokenService, LogoutService, ProfileService, ChangePasswordService
+from .serializers import RegisterSerializer, LoginSerializer, RefreshTokenSerializer, LogoutSerializer, ProfileSerializer, UpdateProfileSerializer, ChangePasswordSerializer, ForgotPasswordSerializer
+from .services import RegistrationService, LoginService, RefreshTokenService, LogoutService, ProfileService, ChangePasswordService, ForgotPasswordService
 
 
 
@@ -202,4 +202,26 @@ class ChangePasswordAPIView(APIView):
         
         return APIResponse.success(
             message="Password changed successfully.",
+        )
+        
+    
+class ForgotPasswordAPIView(APIView):
+
+    permission_classes = [AllowAny]
+
+    @extend_schema(
+        tags=["Authentication"],
+        request=ForgotPasswordSerializer,
+    )
+    def post(self, request):
+
+        serializer = ForgotPasswordSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        ForgotPasswordService.send_reset_email(
+            serializer.validated_data
+        )
+
+        return APIResponse.success(
+            message="Password reset link has been sent to your email.",
         )
