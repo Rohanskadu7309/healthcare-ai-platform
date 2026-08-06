f88a02a23c41c63ca4226d4ece7c18b8348654b9
Rohanskadu7309 <rohanskadu7309@gmai.com>
2026-08-06 20:26:02 +0530

    feat(auth): add resend verification email serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from .models import User
-from .validators import RegistrationValidator, validate_user_email, validate_user_password, validate_confirm_password
+from .validators import validate_user_email, validate_user_password, validate_confirm_password, validate_existing_active_user_email
 
 class RegisterSerializer(serializers.Serializer):
 
@@ -138,8 +138,24 @@ class EmailVerificationSerializer(serializers.Serializer):
     token = serializers.CharField()
     
 
-class ResendEmailVerificationSerializer(serializers.Serializer):
-    
+class ResendVerificationEmailSerializer(serializers.Serializer):
+
     email = serializers.EmailField()
 
-    def
+    def validate(self, attrs):
+
+        email = attrs["email"].strip().lower()
+
+        user = validate_existing_active_user_email(email)
+
+        if user.is_email_verified:
+            raise serializers.ValidationError(
+                {
+                    "email": "Email is already verified."
+                }
+            )
+
+        attrs["email"] = email
+        attrs["user"] = user
+
+        return attrs
