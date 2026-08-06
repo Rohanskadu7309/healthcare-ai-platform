5193945463f3efe4958ea68cbf85f36129f4d8b5
Rohanskadu7309 <rohanskadu7309@gmai.com>
2026-08-06 19:12:30 +0530

    feat(auth): add email verification status to user

--- accounts/models.py
+++ accounts/models.py
@@ -15,6 +15,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=100, blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_email_verified = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
     objects = UserManager()
     USERNAME_FIELD = "email"
